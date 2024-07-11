0556fd543d3c644455a1c1fd7c3cd7f255031877
nazid <nazidk773@gmail.com>
2024-07-11 15:43:31 +0530

    toggle method

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const sidebar = document.querySelector(".sidebar");
 
 toggleBtn.addEventListener("click",function(){
     
-    if(sidebar.classList.contains("show-sidebar")){
-        sidebar.classList.remove("show-sidebar");
-    }
-    else{
-        sidebar.classList.add("show-sidebar")
-    }
+    // if(sidebar.classList.contains("show-sidebar")){
+    //     sidebar.classList.remove("show-sidebar");
+    // }
+    // else{
+    //     sidebar.classList.add("show-sidebar")
+    // }
+
+    sidebar.classList.toggle("show-sidebar");
 });
